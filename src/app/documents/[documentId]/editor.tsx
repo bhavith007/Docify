89c193b0dbdd50ab651e6d540507bc435f17b430
Bhavith <sdbhavith@gmail.com>
2025-02-19 22:11:35 +0530

    Line Height Tool Done

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -18,8 +18,8 @@ import Highlight from "@tiptap/extension-highlight";
 import Link from "@tiptap/extension-link";
 import TextAlign from "@tiptap/extension-text-align";
 
-import {FontSizeExtension} from "@/extensions/font-size";
-
+import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
@@ -54,6 +54,7 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      LineHeightExtension,
       FontSizeExtension,
       TextAlign.configure({
         types: ["heading", "paragraph"],
